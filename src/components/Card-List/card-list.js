57c5d9cd669df78154b27dc71152a99d652b40b4
cantona94 <52317262+cantona94@users.noreply.github.com>
2023-03-31 18:27:40 +0500

    Вёрстка карточки

--- src/components/Card-List/card-list.js
+++ src/components/Card-List/card-list.js
@@ -1,22 +1,28 @@
 import { DivComponent } from '../../common/div-component';
+import { Card } from '../Card/card.js';
 import './card-list.css';
 
 export class CardList extends DivComponent {
-	constructor(appState, parentState) {
-		super();
-		this.appState = appState;
-		this.parentState = parentState;
-	}
+    constructor(appState, parentState) {
+        super();
+        this.appState = appState;
+        this.parentState = parentState;
+    }
 
-	render() {
-		if (this.parentState.loading) {
-			this.el.innerHTML = `<div class="card_list__loader">Загрузка...</div>`;
-			return this.el;
-		}
-		this.el.classList.add('card_list');
-		this.el.innerHTML = `
-			<h1>Найдено книг – ${this.parentState.list.length}</h1>
-		`
-		return this.el;
-	}
+    render() {
+        if (this.parentState.loading) {
+            this.el.innerHTML = `<div class="card_list__loader">Загрузка...</div>`;
+            return this.el;
+        }
+        this.el.classList.add('card_list');
+        this.el.innerHTML = `
+			<h1>Найдено книг - ${this.parentState.numFound}</h1>
+		`;
+
+        for (const card of this.parentState.list) {
+            this.el.append(new Card(this.appState, card).render());
+        }
+
+        return this.el;
+    }
 }
