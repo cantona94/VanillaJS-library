eb37d9bc25c0ac5b1082ad68d086928360c6eec7
cantona94 <52317262+cantona94@users.noreply.github.com>
2023-03-31 17:08:59 +0500

    Компонент Search

--- src/views/main/main.js
+++ src/views/main/main.js
@@ -1,6 +1,7 @@
 import { AbstractView } from '../../common/view.js';
 import onChange from 'on-change';
 import { Header } from '../../components/Header/header.js';
+import { Search } from '../../components/Search/search.js';
 
 export class MainView extends AbstractView {
     state = {
@@ -13,7 +14,8 @@ export class MainView extends AbstractView {
     constructor(appState) {
         super();
         this.appState = appState;
-		this.appState = onChange(this.appState, this.appStateHook.bind(this))
+		this.appState = onChange(this.appState, this.appStateHook.bind(this));
+		this.state = onChange(this.state, this.stateHook.bind(this));
         this.setTitle('Поиск книг');
     }
 
@@ -23,8 +25,23 @@ export class MainView extends AbstractView {
 		}
 	}
 
+	async stateHook(path) {
+		if (path === 'searchQuery') {
+			this.state.loading = true;
+			const data = await this.loadList(this.state.searchQuery, this.state.offset);
+			this.state.loading = false;
+			this.state.list = data.docs;
+		}
+	}
+
+	async loadList(q, offset) {
+		const res = await fetch(`https://openlibrary.org/search.json?q=${q}&offset=${offset}`);
+		return res.json();
+	}
+
     render() {
         const main = document.createElement('div');
+		main.append(new Search(this.state).render());
         this.app.innerHTML = '';
         this.app.append(main);
         this.renderHeader();
